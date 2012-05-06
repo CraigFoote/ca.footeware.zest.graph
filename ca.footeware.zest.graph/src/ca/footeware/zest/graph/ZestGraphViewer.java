cf4ff28a14bbffbdd6b9a7d3e2c139cd0b71a83f
CraigFoote <CraigFoote@gmail.com>
2012-05-06 14:34:45 -0400

    icons

--- ca.footeware.zest.graph/src/ca/footeware/zest/graph/ZestGraphViewer.java
+++ ca.footeware.zest.graph/src/ca/footeware/zest/graph/ZestGraphViewer.java
@@ -38,10 +38,14 @@ public class ZestGraphViewer
 		{
 			if (rel instanceof String)
 			{
+				// if of format "node1 to node2" return "node1"
 				String edgeName = (String) rel;
+				System.err.println("\n getSource: edgeName=" + edgeName);
 				if (edgeName.contains(edgeConjunction))
 				{
-					return edgeName.substring(0, edgeName.indexOf(nodePrefix));
+					String nodeName = edgeName.substring(0, edgeName.indexOf(edgeConjunction));
+					System.err.println(nodeName);
+					return nodeName;
 				}
 			}
 			return null;
@@ -61,10 +65,15 @@ public class ZestGraphViewer
 		{
 			if (rel instanceof String)
 			{
+				// if of format "node1 to node2" return "node2"
 				String edgeName = (String) rel;
+				System.err.println("\ngetDestination: edgeName=" + edgeName);
 				if (edgeName.contains(edgeConjunction))
 				{
-					return edgeName.substring(edgeName.indexOf(edgeConjunction));
+					String nodeName = edgeName.substring(edgeName.indexOf(edgeConjunction)
+							+ edgeConjunction.length());
+					System.err.println(nodeName);
+					return nodeName;
 				}
 			}
 			return rel;
@@ -88,11 +97,16 @@ public class ZestGraphViewer
 	
 	static class MyLabelProvider extends LabelProvider
 	{
-		final Image image = Display.getDefault().getSystemImage(SWT.ICON_INFORMATION);
+		final Image nodeImage = Display.getDefault().getSystemImage(SWT.ICON_INFORMATION);
+		final Image edgeImage = Display.getDefault().getSystemImage(SWT.ICON_QUESTION);
 		
 		public Image getImage(Object element)
 		{
-			return image;
+			if (element instanceof String & ((String) element).contains(edgeConjunction))
+			{
+				return edgeImage;
+			}
+			return nodeImage;
 		}
 		
 		public String getText(Object element)
@@ -111,7 +125,7 @@ public class ZestGraphViewer
 	{
 		Display d = new Display();
 		Shell shell = new Shell(d);
-		shell.setText("GraphJFaceSnippet2");
+		shell.setText("Zest GraphViewer");
 		shell.setLayout(new GridLayout(2, true));
 		shell.setSize(800, 800);
 		
@@ -179,68 +193,22 @@ public class ZestGraphViewer
 		}
 		
 		// make new node
-		System.err.println("creating new node...");
+		System.err.println("\ncreating new node...");
 		List<String> input = (List<String>) viewer.getInput();
 		System.err.println("input=" + input);
 		System.err.println("size=" + input.size());
 		if (input.size() == 0)
 		{
+			System.err.println(nodePrefix + input.size());
 			input.add(nodePrefix + input.size());
 		}
 		else
 		{
+			System.err.println(nodePrefix + input.size() + " to " + nodePrefix
+					+ ((input.size() - 1)));
 			input.add(nodePrefix + input.size() + " to " + nodePrefix + ((input.size() - 1)));
 		}
 		viewer.refresh();
-		// final int newNodeIdx = getRandom(nodes.length, avoidList);
-		// System.err.println("newNodeIdx=" + newNodeIdx);
-		// final GraphNode newNode = new GraphNode(viewer, SWT.NONE, "node " + newNodeIdx);
-		// System.err.println(graph.getNodes());
-		//
-		// // make new connection
-		// System.err.println("creating new connection...");
-		// avoidList = new ArrayList<Integer>();
-		// avoidList.add(new Integer(newNodeIdx));
-		// final int firstConnIdx = getRandom(nodes.size() - 1, avoidList);
-		// System.err.println("firstConnIdx=" + firstConnIdx);
-		// System.err.println(nodes.get(firstConnIdx));
-		// new GraphConnection(graph, SWT.NONE, newNode, (GraphNode) nodes.get(firstConnIdx));
-		//
-		// // randomly make second new connection
-		// if (Math.random() > 0.5)
-		// {
-		// System.err.println("creating second connection");
-		// avoidList = new ArrayList<Integer>();
-		// avoidList.add(new Integer(newNodeIdx));
-		// avoidList.add(new Integer(firstConnIdx));
-		// final int secondConnIdx = getRandom(nodes.size() - 1, avoidList);
-		// System.err.println("secondConnIdx=" + secondConnIdx);
-		// System.err.println(nodes.get(secondConnIdx));
-		// new GraphConnection(graph, SWT.NONE, newNode, (GraphNode) nodes.get(secondConnIdx));
-		// }
 		viewer.applyLayout();
-	}
-	
-	/**
-	 * Generates a random integer between 0 and provided maximum execpt any of the values in
-	 * provided list.
-	 * 
-	 * @param maxNumber
-	 *            int
-	 * @param numbersToAvoid
-	 *            List {@link java.util.List}
-	 * @param numbersToAvoid
-	 */
-	private static int getRandom(final int maxNumber, final List<Integer> numbersToAvoid)
-	{
-		System.err.println("\nmax=" + maxNumber);
-		System.err.println("numbersToAvoid" + numbersToAvoid);
-		int rand;
-		do
-		{
-			rand = (int) Math.round(Math.random() * maxNumber);
-		} while (numbersToAvoid.contains(new Integer(rand)));
-		System.err.println("rand=" + rand + "\n");
-		return rand;
 	}
 }
